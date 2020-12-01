354150ee06c5448ef7582307ad4540e16f462441
Jaren Campbell <jarenrcampbell@gmail.com>
2020-11-30 20:02:29 -0700

    Drones no longer swallowing each other, but only 3/5 drones are able to get going...?

--- sim/FieldObjects.py
+++ sim/FieldObjects.py
@@ -154,28 +154,45 @@ class RandomMovementDrone(Drone):
     def update(self, field):    #update will be passed a copy of the field
         x, y = super().getPos() #get the current position 
 
-        randX = random.randint(-1, 1)
-        randY = random.randint(-1, 1)
-
-        attemptedPosition = field[x + randX][y + randY]
-
-        # If the random direction has an obstacle or drone, try another random direction
-
-        while (isinstance(attemptedPosition, (Obstacle, Drone)) 
-                or x + randX < 0 or x + randX > len(field) 
-                or y + randY < 0 or y + randY > len(field[0])):
-            randX = random.randint(-1, 1)
-            randY = random.randint(-1, 1)
-            attemptedPosition = field[x + randX][y + randY]
+        possible_moves = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
         
-        # Case where goal is found
-        if isinstance(field[x + randX][y + randY], Goal):
-            self.setSymbol("X")
-            return True
-        else:
-            x += randX
-            y += randY
-        super().update(x, y)
+
+        finalX = x
+        finalY = y
+        good_move = False
+
+        while not good_move and len(possible_moves) > 0:
+            chosen_move = random.randint(0, len(possible_moves) - 1)
+            newX, newY = possible_moves[chosen_move]
+
+            if newX < 0 or newX >= len(field) or newY < 0 or newY >= len(field[0]):
+                possible_moves.pop(chosen_move)
+            
+            elif isinstance(field[newX][newY], Goal):
+                self.setSymbol("X")
+                return True
+
+            elif self.checkForDrones(field, x, y, newX, newY) or isinstance(field[newX][newY], Obstacle):
+                possible_moves.pop(chosen_move)
+
+            else:
+                finalX = newX
+                finalY = newY
+                good_move = True
+        
+        super().update(finalX, finalY)
+        return False
+
+    def checkForDrones(self, field, x, y, newX, newY):
+        for i in range(newX - 1, newX + 2):
+            for j in range(newY - 1, newY + 2):
+                if i == x and j == y:
+                    continue
+                if i < 0 or i >= len(field) or j < 0 or j >= len(field[0]):
+                    continue
+                if isinstance(field[i][j], (Drone)):
+                    return True
+        
         return False
 
 
